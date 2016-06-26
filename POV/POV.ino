ea9934da6e4f77955aa8b9ffbe75024582752df3
AliReza Abdiyan <alireza.abdiyan@gmail.com>
2016-06-26 14:58:40 +0430

    Update POV.ino

--- POV/POV.ino
+++ POV/POV.ino
@@ -1,3 +1,16 @@
+/*
+  Copyright (c) 2015, Arduino LLC
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  
+  by:AliReza.Abdiyan@gmail.com
+*/
+
 #include <Arduino.h>
 #include <SPI.h>
 #include <FastLED.h>
